925a219c76558d7335a450ab19fead9946bfed45
Ethan Ferguson <eferg00@gmail.com>
2024-10-03 23:45:18 -0600

    Fixed AWS bucket name in secrets

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           GROTTO DIGITAL
         </p>
+
       </header>
     </div>
   );
